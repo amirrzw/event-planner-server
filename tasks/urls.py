39377dc2c51e505a6980f7bd7a2a38e5af46c924
amirrzw <amirrezaabotalebi81@gmail.com>
2024-07-02 12:23:57 +0330

    add automatic scheduling

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, TaskViewSet
+from .views import CategoryViewSet, TaskViewSet, schedule_tasks
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
@@ -8,4 +8,5 @@ router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('schedule/', schedule_tasks, name='schedule-tasks'),
 ]
